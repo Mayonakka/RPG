91e1d5a20f3372ca7eaba4d48fccdb938cec1548
Arthur de Oliveira <>
2021-04-03 18:50:04 +0000

    Progresso inicial

--- Personagem.java
+++ Personagem.java
@@ -53,10 +53,10 @@ public abstract class Personagem {
   }
   
   public abstract void passaTurno(){
-
+    
   }
 
-  public abstract void ataca(){
+  public abstract void ataca(Personagem p){
 
   }
   
